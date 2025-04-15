1d0498d46ffe3b568d9dc2bd87814da2a5bef8df
Fernando-Espinosa <fernando.espinosadgo@gmail.com>
2025-04-15 01:24:46 -0600

    Integrate vite plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import generateFile from 'vite-plugin-generate-file';
+import { qrcode } from 'vite-plugin-qrcode';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
+  plugins: [
+    react(),
+    generateFile([
+      {
+        type: 'json',
+        output: './build-info.json',
+        data: {
+          buildTime: new Date().toISOString(),
+          version: process.env.npm_package_version,
+        },
+      },
+    ]),
+    qrcode(),
+  ],
 });
